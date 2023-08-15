1016d56b5ddf10e104a37828912deed039c96fad
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-15 23:50:14 +0300

    Create 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS first_table
+(
+    id INT,
+    name  VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table(id,name,SCORE) VALUES 
+(1,"John",10),
+(2,"Alex",3),
+(3,"Bob",14),
+(4,"George",8);
