953b8b2238cad0b9c0e4e2e404df7cd7f9b53727
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-16 17:54:57 +0300

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- SELECT ALL VALUES WHERE THE NAME IS CALIFORINA AND IRS ORDERED
 
 SELECT * 
-FROM states cities 
+FROM states 
 WHERE name = 'California' 
 ORDER BY (SELECT id FROM cities WHERE cities.state_id = states.id);
