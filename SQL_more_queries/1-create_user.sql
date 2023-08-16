bb2c8e70504cd7bc0bbb1e4bed8ff9a17a75ba0f
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-16 18:14:03 +0300

    spelling error fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost'='user_0d_1_pwd';
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVELAGES;
+FLUSH PRIVILAGES;
