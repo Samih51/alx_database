c6698ce53a1f73ad1101dd7e2b75c6a1fc4f41bd
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-16 17:09:54 +0300

    Create 2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- CREATE A DATABASE AND USER THAT ONLY READS THE DATABASE
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVELAGES;
