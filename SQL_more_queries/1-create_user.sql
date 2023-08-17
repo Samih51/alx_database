80e890b2ea5d13870da238bbc56c84f147e7f07c
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-08-17 19:59:49 +0300

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost'='user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILAGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
